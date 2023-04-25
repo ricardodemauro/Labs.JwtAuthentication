c3a5e91e1d9d92e18c3b2ea65e646d5b08ec4f1c
Ricardo <ricardo.demauro@gmail.com>
2023-04-25 13:21:31 -0400

    :constrution: refactoring tokenendpoint to pass the expiration instead of receiving it

--- Labs.JwtAuthentication/Endpoints/TokenEndpoint.cs
+++ Labs.JwtAuthentication/Endpoints/TokenEndpoint.cs
@@ -8,72 +8,72 @@ using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Labs.JwtAuthentication.Endpoints
+namespace Labs.JwtAuthentication.Endpoints;
+
+public static class TokenEndpoint
 {
-    public static class TokenEndpoint
+    public static async Task<IResult> Connect(
+        HttpContext ctx,
+        JwtOptions jwtOptions)
     {
-        internal static async Task<IResult> Connect(
-            HttpContext ctx,
-            JwtOptions jwtOptions)
+        if (ctx.Request.ContentType != "application/x-www-form-urlencoded")
+            return Results.BadRequest(new { Error = "Invalid Request" });
+
+        var formCollection = await ctx.Request.ReadFormAsync();
+
+        if (formCollection.TryGetValue("grant_type", out var grantType) == false)
+            return Results.BadRequest(new { Error = "Invalid Request" });
+
+        if (formCollection.TryGetValue("username", out var userName) == false)
+            return Results.BadRequest(new { Error = "Invalid Request" });
+
+        if (formCollection.TryGetValue("password", out var password) == false)
+            return Results.BadRequest(new { Error = "Invalid Request" });
+
+        var (accessToken, expiration) = TokenEndpoint.CreateAccessToken(
+            jwtOptions,
+            userName,
+            new[] { "read_todo", "create_todo" });
+
+        var exp = expiration - DateTime.Now;
+        return Results.Ok(new
         {
-            if (ctx.Request.ContentType != "application/x-www-form-urlencoded")
-                return Results.BadRequest(new { Error = "Invalid Request" });
+            access_token = accessToken,
+            expiration = (int)double.Round(exp.TotalSeconds, 0, MidpointRounding.ToZero),
+            type = "bearer"
+        });
+    }
 
-            var formCollection = await ctx.Request.ReadFormAsync();
+    static string CreateAccessToken(
+        JwtOptions jwtOptions,
+        string username,
+        TimeSpan expiration,
+        string[] permissions)
+    {
+        var keyBytes = Encoding.UTF8.GetBytes(jwtOptions.SigningKey);
+        var symmetricKey = new SymmetricSecurityKey(keyBytes);
+        var expiration = DateTime.Now.AddMinutes(15);
 
-            if (formCollection.TryGetValue("grant_type", out var grantType) == false)
-                return Results.BadRequest(new { Error = "Invalid Request" });
+        var signingCredentials = new SigningCredentials(
+            symmetricKey,
+            SecurityAlgorithms.HmacSha256);
 
-            if (formCollection.TryGetValue("username", out var userName) == false)
-                return Results.BadRequest(new { Error = "Invalid Request" });
-
-            if (formCollection.TryGetValue("password", out var password) == false)
-                return Results.BadRequest(new { Error = "Invalid Request" });
-
-            var (accessToken, expiration) = TokenEndpoint.CreateAccessToken(
-                jwtOptions,
-                userName,
-                new[] { "read_todo", "create_todo" });
-
-            var exp = expiration - DateTime.Now;
-            return Results.Ok(new
-            {
-                access_token = accessToken,
-                expiration = (int)double.Round(exp.TotalSeconds, 0, MidpointRounding.ToZero),
-                type = "bearer"
-            });
-        }
-
-        static (string, DateTime) CreateAccessToken(
-            JwtOptions jwtOptions,
-            string username,
-            string[] permissions)
+        var claims = new[]
         {
-            var keyBytes = Encoding.UTF8.GetBytes(jwtOptions.SigningKey);
-            var symmetricKey = new SymmetricSecurityKey(keyBytes);
-            var expiration = DateTime.Now.AddMinutes(15);
+            new Claim(JwtClaimTypes.Subject, username),
+            new Claim(JwtClaimTypes.Name, username),
+            new Claim(JwtClaimTypes.Role, permissions[0]),
+            new Claim(JwtClaimTypes.Audience, jwtOptions.Audience)
+        };
 
-            var signingCredentials = new SigningCredentials(
-                symmetricKey,
-                SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: jwtOptions.Issuer,
+            audience: jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddHours(1),
+            signingCredentials: signingCredentials);
 
-            var claims = new[]
-            {
-                new Claim(JwtClaimTypes.Subject, username),
-                new Claim(JwtClaimTypes.Name, username),
-                new Claim(JwtClaimTypes.Role, permissions[0]),
-                new Claim(JwtClaimTypes.Audience, jwtOptions.Audience)
-            };
-
-            var token = new JwtSecurityToken(
-                issuer: jwtOptions.Issuer,
-                audience: jwtOptions.Audience,
-                claims: claims,
-                expires: DateTime.Now.AddHours(1),
-                signingCredentials: signingCredentials);
-
-            var rawToken = new JwtSecurityTokenHandler().WriteToken(token);
-            return (rawToken, expiration);
-        }
+        var rawToken = new JwtSecurityTokenHandler().WriteToken(token);
+        return (rawToken, expiration);
     }
 }
