0c2a17fd4a1561fddc63730bf7d628fb5760fd73
Ricardo Mauro <ricardo.demauro@outlook.com>
2023-05-29 19:33:45 -0400

    adding methods to read jwt token from incoming request

--- Labs.JwtAuthentication/Program.cs
+++ Labs.JwtAuthentication/Program.cs
@@ -1,6 +1,8 @@
 using Labs.JwtAuthentication;
 using Labs.JwtAuthentication.Endpoints;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -38,5 +40,24 @@ app.MapGet("/private", () => "Private Hello World!").RequireAuthorization();
 
 app.MapPost("/tokens/connect", (HttpContext ctx, JwtOptions jwtOptions)
     => TokenEndpoint.Connect(ctx, jwtOptions));
+
+//read the jwt token from header
+app.MapGet("/jwt-token/headers", (HttpContext ctx) =>
+{
+    if (ctx.Request.Headers.TryGetValue("Authorization", out var headerAuth))
+    {
+        var jwtToken = headerAuth.First().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[1];
+        return Task.FromResult(TypedResults.Ok(new { token = jwtToken }));
+    }
+    return Task.FromResult(TypedResults.NotFound(new { message = "jwt not found" }));
+});
+
+//read the jwt token from authentication context
+app.MapGet("/jwt-token/context", async (HttpContext ctx) =>
+{
+    var token = await ctx.GetTokenAsync("access_token");
+
+    return TypedResults.Ok(new { token = token });
+});
 
 app.Run();
